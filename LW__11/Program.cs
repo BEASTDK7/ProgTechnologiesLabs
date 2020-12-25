f59daf542a567a6d3b7ec8aa0bc8dd7fc2801774
BEASTDK7 <72353569+BEASTDK7@users.noreply.github.com>
2020-12-25 16:50:29 +0300

    Add files via upload

---
+++ LW__11/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LW__11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader f_in = new StreamReader("lr11_05.csv");
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            var new_out = new StreamWriter(@"lr11_output.txt");
+            Console.SetOut(new_out);
+#endif
+
+            List<Ticket> all = new List<Ticket>();
+
+            try
+            {
+                String line = f_in.ReadLine();
+                while ((line = f_in.ReadLine()) != null)
+                {
+                    all.Add(Ticket.Create(line));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            //foreach (Ticket t in all)
+            //    Console.WriteLine(t);
+
+            Console.WriteLine("******************************* Задача 1 *******************************");
+            Console.WriteLine("******* Количество билетов с пунктом отправления из Ставрополя *******\n");
+            int total_from_stavropol = (from t in all
+                                        where (t.CityFrom == City.Stavropol) select t).Count();
+            Console.WriteLine(String.Format("Кол-во билетов из Ставрополя: {0}", total_from_stavropol));
+            Console.WriteLine("\n\n");
+
+
+            Console.WriteLine("******************************* Задача 2 *******************************");
+            Console.WriteLine("******* Средняя цена билета для каждого из пунктов отправления *******\n");
+            float average_price_from_moscow = (from t in all
+                                                  where (t.CityFrom == City.Moscow)
+                                                  select t.Price).Average();
+            float average_price_from_stavropol = (from t in all
+                                                  where (t.CityFrom == City.Stavropol)
+                                                  select t.Price).Average();
+            float average_price_from_voronezh = (from t in all
+                                                  where (t.CityFrom == City.Voronezh)
+                                                  select t.Price).Average();
+            float average_price_from_rostov = (from t in all
+                                                  where (t.CityFrom == City.Rostov_on_Don)
+                                                  select t.Price).Average();
+
+            Console.WriteLine(String.Format("Средняя цена отправления из Москвы: ${0:0.00}", average_price_from_moscow));
+            Console.WriteLine(String.Format("Средняя цена отправления из Ставрополя: ${0:0.00}", average_price_from_stavropol));
+            Console.WriteLine(String.Format("Средняя цена отправления из Воронежа: ${0:0.00}", average_price_from_voronezh));
+            Console.WriteLine(String.Format("Средняя цена отправления из Ростова-на-Дону: ${0:0.00}", average_price_from_rostov));
+            Console.WriteLine("\n\n");
+
+
+            Console.WriteLine("******************************* Задача 3 *******************************");
+            Console.WriteLine("******* Кол-во билетов, купленных женщинами и мужчинами *******\n");
+            int male_tickets = (from t in all
+                                where (t.Gender == GenderType.Male)
+                                select t).Count();
+            int female_tickets = all.Count - male_tickets;
+
+            Console.WriteLine(String.Format("Количество билетов, купленных мужчинами: {0} шт.", male_tickets));
+            Console.WriteLine(String.Format("Количество билетов, купленных женщинами: {0} шт.", female_tickets));
+            Console.WriteLine("\n\n");
+
+
+            Console.WriteLine("******************************* Задача 4 *******************************");
+            Console.WriteLine("******* Кол-во билетов с одинаковыми пунктами отправления и прибытия *******\n");
+            int total_domestic = (from t in all
+                                  where (t.CityFrom == t.CityTo)
+                                  select t).Count();
+            Console.WriteLine(String.Format("Количество билетов внутренних перелетов: {0} шт.", total_domestic));
+
+#if !DEBUG
+            Console.SetOut(save_out);
+            new_out.Close();
+#endif
+        }
+    }
+}
---
+++ LW__11/Ticket.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LW__11
+{
+    enum City
+    {
+        Moscow,
+        Stavropol,
+        Voronezh,
+        Rostov_on_Don
+    }
+
+    enum GenderType
+    {
+        Male,
+        Female
+    }
+
+    class Ticket
+    {
+        String ID { get; set; }
+        String Name { get; set; }
+        public GenderType Gender { get; set; }
+        public City CityFrom { get; set; }
+        public City CityTo { get; set; }
+        public float Price { get; set; }
+        
+
+        public static Ticket Create(String str)
+        {
+            Ticket t = new Ticket();
+            string[] e = str.Split(',');
+            t.ID = e[0].Trim();
+            t.Name = e[1].Trim();
+            String tmp = e[2].Trim();
+            if (tmp == "M")
+                t.Gender = GenderType.Male;
+            else t.Gender = GenderType.Female;
+
+            tmp = e[3].Trim();
+            if (tmp == "Moscow")
+                t.CityFrom = City.Moscow;
+            else if (tmp == "Stavropol")
+                t.CityFrom = City.Stavropol;
+            else if (tmp == "Voronezh")
+                t.CityFrom = City.Voronezh;
+            else
+                t.CityFrom = City.Rostov_on_Don;
+
+            tmp = e[4].Trim();
+            if (tmp == "Moscow")
+                t.CityTo = City.Moscow;
+            else if (tmp == "Stavropol")
+                t.CityTo = City.Stavropol;
+            else if (tmp == "Voronezh")
+                t.CityTo = City.Voronezh;
+            else
+                t.CityTo = City.Rostov_on_Don;
+
+            t.Price = Convert.ToSingle(e[5].TrimStart('$').Replace('.', ','));
+            return t;
+        }
+
+        private static String CityToStr(City c)
+        {
+            if (c == City.Moscow) return "Moscow";
+            else if (c == City.Stavropol) return "Stavropol";
+            else if (c == City.Voronezh) return "Voronezh";
+            else return "Rostov-on-Don";
+        }
+
+        private static String GenderToStr(GenderType g)
+        {
+            if (g == GenderType.Male) return "М";
+            else return "Ж";
+        }
+
+        public override string ToString()
+        {
+            String s = string.Format(
+                "********************************************************\n" +
+                "ID: {0}, Имя держателя билета: {1}\n" +
+                "Пол: {2}\n" +
+                "Город отправления: {3}\n" +
+                "Город прибытия: {4}\n " +
+                "Цена: ${5}\n" +
+                "********************************************************\n",
+                ID, Name, GenderToStr(Gender), CityFrom, CityTo, Price);
+            return s;
+        }
+    }
+}
