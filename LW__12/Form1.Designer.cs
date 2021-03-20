2c26c2880b6c7658951f00f87d08bca14d8ac9ac
BEASTDK7 <72353569+BEASTDK7@users.noreply.github.com>
2021-03-20 23:35:08 +0300

    Add files via upload

---
+++ LW__12/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace LW__12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxE = new System.Windows.Forms.TextBox();
+            this.textBoxT = new System.Windows.Forms.TextBox();
+            this.textBoxF = new System.Windows.Forms.TextBox();
+            this.textBoxW = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxE
+            // 
+            this.textBoxE.Location = new System.Drawing.Point(462, 53);
+            this.textBoxE.Name = "textBoxE";
+            this.textBoxE.Size = new System.Drawing.Size(100, 20);
+            this.textBoxE.TabIndex = 0;
+            this.textBoxE.TextChanged += new System.EventHandler(this.textBoxE_TextChanged);
+            // 
+            // textBoxT
+            // 
+            this.textBoxT.Location = new System.Drawing.Point(462, 96);
+            this.textBoxT.Name = "textBoxT";
+            this.textBoxT.Size = new System.Drawing.Size(100, 20);
+            this.textBoxT.TabIndex = 1;
+            this.textBoxT.TextChanged += new System.EventHandler(this.textBoxT_TextChanged);
+            // 
+            // textBoxF
+            // 
+            this.textBoxF.Location = new System.Drawing.Point(258, 53);
+            this.textBoxF.Name = "textBoxF";
+            this.textBoxF.ReadOnly = true;
+            this.textBoxF.Size = new System.Drawing.Size(100, 20);
+            this.textBoxF.TabIndex = 2;
+            // 
+            // textBoxW
+            // 
+            this.textBoxW.Location = new System.Drawing.Point(258, 96);
+            this.textBoxW.Name = "textBoxW";
+            this.textBoxW.ReadOnly = true;
+            this.textBoxW.Size = new System.Drawing.Size(100, 20);
+            this.textBoxW.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(233, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "f =";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(228, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "w =";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(434, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "e =";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(437, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "t =";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxW);
+            this.Controls.Add(this.textBoxF);
+            this.Controls.Add(this.textBoxT);
+            this.Controls.Add(this.textBoxE);
+            this.Name = "Form1";
+            this.Text = "ИНС191 Кошевой ЛР12";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.TextChanged += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxE;
+        private System.Windows.Forms.TextBox textBoxT;
+        private System.Windows.Forms.TextBox textBoxF;
+        private System.Windows.Forms.TextBox textBoxW;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ LW__12/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LW__12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        float f, w, my_e, my_t;
+        double Tt;
+
+        private void textBoxE_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                my_e = System.Convert.ToSingle(textBoxE.Text);
+                Tt = GetValue(f, w, my_e, my_t);
+                Text = GetTitleResult(Tt);
+            }
+
+            catch
+            {
+                Text = "ERROR";
+            }
+        }
+
+        private void textBoxT_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                my_t = System.Convert.ToSingle(textBoxT.Text);
+                Tt = GetValue(f, w, my_e, my_t);
+                Text = GetTitleResult(Tt);
+            }
+
+            catch
+            {
+                Text = "ERROR";
+            }
+        }
+                                     
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            f = e.X;
+            w = e.Y;
+            Tt = GetValue(f, w, my_e, my_t);
+            Text = GetTitleResult(Tt);
+
+            textBoxF.Text = e.X.ToString();
+            textBoxW.Text = e.Y.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        double GetValue(float f, float w, float e, float t)
+        {
+            double result = Math.Log10(f) - e + Math.Abs(Math.Sin(w / t) + Math.Sqrt(Math.Abs(e)));
+            return result;
+        }
+
+        string GetTitleResult(double X)
+        {
+            if (Double.IsInfinity(X) || Double.IsNaN(X))
+            {
+                return "ERROR";
+            }
+            else
+            {
+                return string.Format("t = {0:0.00}", X);
+            }
+        }
+    }
+}
