0a0f447e1868ffb9fd7c25a0a987fbb1d36023ea
BEASTDK7 <72353569+BEASTDK7@users.noreply.github.com>
2021-03-23 22:28:10 +0300

    Add files via upload

---
+++ LW__15/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿
+namespace LW__15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChooseColor = new System.Windows.Forms.Button();
+            this.btnChooseFont = new System.Windows.Forms.Button();
+            this.btnChooseFolder = new System.Windows.Forms.Button();
+            this.ButtonResult = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.labelForU = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.textBoxQnt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panelChooseColor = new System.Windows.Forms.Panel();
+            this.txtChooseFont = new System.Windows.Forms.TextBox();
+            this.txtChooseFolder = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnChooseColor
+            // 
+            this.btnChooseColor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnChooseColor.Location = new System.Drawing.Point(17, 24);
+            this.btnChooseColor.Name = "btnChooseColor";
+            this.btnChooseColor.Size = new System.Drawing.Size(100, 41);
+            this.btnChooseColor.TabIndex = 0;
+            this.btnChooseColor.Text = "Выбор цвета фона";
+            this.btnChooseColor.UseVisualStyleBackColor = false;
+            this.btnChooseColor.Click += new System.EventHandler(this.btnChooseColor_Click);
+            // 
+            // btnChooseFont
+            // 
+            this.btnChooseFont.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnChooseFont.Location = new System.Drawing.Point(17, 105);
+            this.btnChooseFont.Name = "btnChooseFont";
+            this.btnChooseFont.Size = new System.Drawing.Size(100, 23);
+            this.btnChooseFont.TabIndex = 1;
+            this.btnChooseFont.Text = "Выбор шрифта";
+            this.btnChooseFont.UseVisualStyleBackColor = false;
+            this.btnChooseFont.Click += new System.EventHandler(this.btnChooseFont_Click);
+            // 
+            // btnChooseFolder
+            // 
+            this.btnChooseFolder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnChooseFolder.Location = new System.Drawing.Point(17, 169);
+            this.btnChooseFolder.Name = "btnChooseFolder";
+            this.btnChooseFolder.Size = new System.Drawing.Size(100, 23);
+            this.btnChooseFolder.TabIndex = 2;
+            this.btnChooseFolder.Text = "Выбор каталога";
+            this.btnChooseFolder.UseVisualStyleBackColor = false;
+            this.btnChooseFolder.Click += new System.EventHandler(this.btnChooseFolder_Click);
+            // 
+            // ButtonResult
+            // 
+            this.ButtonResult.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ButtonResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ButtonResult.Location = new System.Drawing.Point(311, 143);
+            this.ButtonResult.Name = "ButtonResult";
+            this.ButtonResult.Size = new System.Drawing.Size(516, 49);
+            this.ButtonResult.TabIndex = 3;
+            this.ButtonResult.Text = "Решить!";
+            this.ButtonResult.UseVisualStyleBackColor = false;
+            this.ButtonResult.Click += new System.EventHandler(this.ButtonResult_Click);
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.Color = System.Drawing.Color.White;
+            // 
+            // labelForU
+            // 
+            this.labelForU.AutoSize = true;
+            this.labelForU.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelForU.Location = new System.Drawing.Point(6, 37);
+            this.labelForU.Name = "labelForU";
+            this.labelForU.Size = new System.Drawing.Size(389, 24);
+            this.labelForU.TabIndex = 5;
+            this.labelForU.Text = "Enter values, then click the button above";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LW__15.Properties.Resources.ForU;
+            this.pictureBox1.Location = new System.Drawing.Point(311, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(516, 64);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(346, 108);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(100, 20);
+            this.textBoxX.TabIndex = 7;
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(509, 108);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(100, 20);
+            this.textBoxY.TabIndex = 8;
+            // 
+            // textBoxQnt
+            // 
+            this.textBoxQnt.Location = new System.Drawing.Point(727, 108);
+            this.textBoxQnt.Name = "textBoxQnt";
+            this.textBoxQnt.Size = new System.Drawing.Size(100, 20);
+            this.textBoxQnt.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(307, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "x = ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(470, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "y = ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(640, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Quantity =";
+            // 
+            // panelChooseColor
+            // 
+            this.panelChooseColor.Location = new System.Drawing.Point(138, 24);
+            this.panelChooseColor.Name = "panelChooseColor";
+            this.panelChooseColor.Size = new System.Drawing.Size(127, 41);
+            this.panelChooseColor.TabIndex = 13;
+            // 
+            // txtChooseFont
+            // 
+            this.txtChooseFont.Location = new System.Drawing.Point(138, 107);
+            this.txtChooseFont.Name = "txtChooseFont";
+            this.txtChooseFont.ReadOnly = true;
+            this.txtChooseFont.Size = new System.Drawing.Size(127, 20);
+            this.txtChooseFont.TabIndex = 14;
+            this.txtChooseFont.Text = "Example";
+            // 
+            // txtChooseFolder
+            // 
+            this.txtChooseFolder.Location = new System.Drawing.Point(138, 171);
+            this.txtChooseFolder.Name = "txtChooseFolder";
+            this.txtChooseFolder.Size = new System.Drawing.Size(127, 20);
+            this.txtChooseFolder.TabIndex = 15;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.labelForU);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(17, 212);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(810, 97);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(845, 332);
+            this.Controls.Add(this.txtChooseFolder);
+            this.Controls.Add(this.txtChooseFont);
+            this.Controls.Add(this.panelChooseColor);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxQnt);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ButtonResult);
+            this.Controls.Add(this.btnChooseFolder);
+            this.Controls.Add(this.btnChooseFont);
+            this.Controls.Add(this.btnChooseColor);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "ИНС191 Кошевой ЛР15";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChooseColor;
+        private System.Windows.Forms.Button btnChooseFont;
+        private System.Windows.Forms.Button btnChooseFolder;
+        private System.Windows.Forms.Button ButtonResult;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label labelForU;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.TextBox textBoxQnt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panelChooseColor;
+        private System.Windows.Forms.TextBox txtChooseFont;
+        private System.Windows.Forms.TextBox txtChooseFolder;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ LW__15/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LW__15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChooseColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog dlg = new ColorDialog();
+            dlg.FullOpen = true;
+            dlg.ShowHelp = true;
+            dlg.Color = panelChooseColor.BackColor;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                panelChooseColor.BackColor = dlg.Color;
+                labelForU.BackColor = dlg.Color;
+            }
+        }
+
+        private void btnChooseFont_Click(object sender, EventArgs e)
+        {
+            FontDialog dlg = new FontDialog();
+            dlg.ShowColor = true;
+            dlg.ShowHelp = true;
+            dlg.Font = txtChooseFont.Font;
+            
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                txtChooseFont.Font = dlg.Font;
+                labelForU.Font = dlg.Font;
+                txtChooseFont.ForeColor = dlg.Color;
+                labelForU.ForeColor = dlg.Color;
+            }
+        }
+
+        private void btnChooseFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            dlg.Description = "Выберите каталог";
+            dlg.ShowNewFolderButton = true;
+            dlg.SelectedPath = Application.StartupPath;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                txtChooseFolder.Text = dlg.SelectedPath;
+            }
+        }
+
+        private void ButtonResult_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double x = Convert.ToDouble(textBoxX.Text);
+                double y = Convert.ToDouble(textBoxY.Text);
+                double Qnt = Convert.ToDouble(textBoxQnt.Text);
+
+                double U = 0; int j = 1;
+
+                if (Qnt > 0)
+                {
+                    for (int i = 1; i <= Qnt; i++)
+                    {
+                        U += Math.Pow(-Math.Sin(x), i + 1) * Math.Pow(y, j) / (i + 1);
+                        j += 2;
+                    }
+
+                    labelForU.Text = String.Format("U = {0:0.000}", 1 + U);
+                }
+                                
+                if (txtChooseFolder.Text != "")
+                {
+                    string file = Path.Combine(txtChooseFolder.Text, "LW15_Result.txt");
+                    File.WriteAllText(file, Convert.ToString(U));
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Некорректный ввод!", "Oops!");
+            }
+        }
+    }
+}
